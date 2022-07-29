d0afb54b6b85a9579e12da2f4fc89c92515095ec
Tim Morgner <tim@timmorgner.de>
2022-07-29 23:14:45 +0200

    fixed sender protocol

--- src/signal_cli/sender.py
+++ src/signal_cli/sender.py
@@ -2,7 +2,7 @@ import logging
 
 import requests
 
-from util.constants import TELNUMBER, HOSTNAME
+from util.constants import TELNUMBER, HOSTNAME, PROTOCOL
 
 logger = logging.getLogger("sender")
 
@@ -15,6 +15,8 @@ def sendText(recepient, message):
             f"{recepient}"
         ]
     }
-    logger.debug("Sent message '%s' to %s", message, recepient)
-    post_response = requests.post(f"https://{HOSTNAME}/v2/send", json=body,timeout=60)
+    uri = f"{PROTOCOL}://{HOSTNAME}/v2/send"
+    logger.debug("Sending message '%s' to %s", message, recepient)
+    logger.debug("Sending message via %s", uri)
+    post_response = requests.post(uri, json=body,timeout=60)
     logger.debug("Response: %s", post_response)
