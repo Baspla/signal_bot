67d74b2e44df2812b4449d6966cb5a5e44c3b397
Tim Morgner <tim@timmorgner.de>
2022-07-29 22:56:49 +0200

    more logs

--- src/util/constants.py
+++ src/util/constants.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+_logger = logging.getLogger("constants")
+
 HOSTNAME = os.environ.get('SIGNAL_HOST', "localhost")
 
 TELNUMBER = os.environ.get('SIGNAL_NUMBER')
@@ -12,3 +14,7 @@ POLLING_INTERVAL = int(os.environ.get('POLLING_INTERVAL', "15"))
 LOGGING_LEVEL = os.environ.get('LOG_LEVEL', "INFO")
 
 PROTOCOL = os.environ.get('SIGNAL_PROTOCOL', "https")
+
+def printAllConstants():
+    _logger.info("Config: SIGNAL_HOST = %s, SIGNAL_NUMBER = %s, SIGNAL_PROTOCOL = %s,"
+                 " POLLING_INTERVAL = %s, LOG_LEVEL = %s",HOSTNAME,TELNUMBER,PROTOCOL,POLLING_INTERVAL,LOGGING_LEVEL)
